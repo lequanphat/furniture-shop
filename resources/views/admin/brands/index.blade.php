24741ee07be4059de2c8a623bdf47f4d0681027d
lequanphat <lequanphat3579@gmail.com>
2024-03-04 20:45:10 +0700

    build directory structure

--- resources/views/admin/brands/index.blade.php
+++ resources/views/admin/brands/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 @section('content')
     <section id="main-content">
-        <h1>This is permissions page</h1>
+        <h1>This is brands page</h1>
         @include('admin.components.footer')
     </section>
 @endsection
