ccf3fa5fce1ff9e19fff1d59bb05a283e5048928
lequanphat <lequanphat3579@gmail.com>
2024-03-08 08:52:00 +0700

    create employee with ajax

--- resources/views/admin/components/head.blade.php
+++ resources/views/admin/components/head.blade.php
@@ -20,7 +20,6 @@
     <link href="{{ asset('assets/css/lib/themify-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/lib/owl.carousel.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/lib/owl.theme.default.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/lib/weather-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/lib/menubar/sidebar.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/lib/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/lib/helper.css') }}" rel="stylesheet">
--- resources/views/admin/components/link-script.blade.php
+++ resources/views/admin/components/link-script.blade.php
@@ -1,33 +1,26 @@
 <!-- jquery vendor -->
-<script src="{{ asset('assets/js/lib/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/jquery.nanoscroller.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/jquery.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/jquery.nanoscroller.min.js') }}" defer></script>
 <!-- nano scroller -->
-<script src="{{ asset('assets/js/lib/menubar/sidebar.js') }}"></script>
-<script src="{{ asset('assets/js/lib/preloader/pace.min.js') }}"></script>
+<script src="{{ asset('assets/js/lib/menubar/sidebar.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/preloader/pace.min.js') }}" defer></script>
 <!-- sidebar -->
-<script src="{{ asset('assets/js/lib/calendar-2/moment.latest.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/calendar-2/pignose.calendar.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/calendar-2/pignose.init.js') }}"></script>
-<script src="{{ asset('assets/js/lib/chartist/chartist.min.js') }}"></script>
 {{-- slide  --}}
-<script src="{{ asset('assets/js/lib/owl-carousel/owl.carousel.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/owl-carousel/owl.carousel-init.js') }}"></script>
+<script src="{{ asset('assets/js/lib/owl-carousel/owl.carousel.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/owl-carousel/owl.carousel-init.js') }}" defer></script>
 {{-- for user1 --}}
-<script src="{{ asset('assets/js/lib/data-table/datatables.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/dataTables.buttons.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/buttons.flash.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/jszip.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/pdfmake.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/vfs_fonts.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/buttons.html5.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/buttons.print.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/data-table/datatables-init.js') }}"></script>
-{{-- chartJS --}}
-<script src="{{ asset('assets/js/lib/chart-js/Chart.bundle.js') }}"></script>
-<script src="{{ asset('assets/js/lib/chart-js/chartjs-init.js') }}"></script>
+<script src="{{ asset('assets/js/lib/data-table/datatables.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/dataTables.buttons.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/buttons.flash.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/jszip.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/pdfmake.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/vfs_fonts.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/buttons.html5.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/buttons.print.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/data-table/datatables-init.js') }}" defer></script>
 <!-- scripit init-->
-<script src="assets/js/dashboard.js"></script>
-<script src="{{ asset('assets/js/lib/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/js/lib/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('assets/js/scripts.js') }}"></script>
+<script src="{{ asset('assets/js/lib/bootstrap.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/lib/bootstrap.bundle.min.js') }}" defer></script>
+<script src="{{ asset('assets/js/scripts.js') }}" defer></script>
+<script src="{{ asset('assets/js/api.js') }}" defer></script>
 <!-- bootstrap -->
