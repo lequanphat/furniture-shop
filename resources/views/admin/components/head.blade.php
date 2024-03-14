8a0e1b7223c7a7dea14500ae505b3a185aae99a7
lequanphat <lequanphat3579@gmail.com>
2024-03-14 19:25:24 +0700

    admin ui using tabler

--- resources/views/admin/components/head.blade.php
+++ resources/views/admin/components/head.blade.php
@@ -1,27 +1,19 @@
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin - Firnuture</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Dashboard</title>
+    <!-- CSS files -->
+    <link rel="stylesheet" href="{{ asset('vendor/tabler/tabler.min.css') }}">
+    <style>
+        @import url('https://rsms.me/inter/inter.css');
 
-    <link rel="shortcut icon" href="favicon.png">
-    <!-- Retina iPad Touch Icon-->
-    <link rel="apple-touch-icon" sizes="144x144" href="http://placehold.it/144.png/000/fff">
-    <!-- Retina iPhone Touch Icon-->
-    <link rel="apple-touch-icon" sizes="114x114" href="http://placehold.it/114.png/000/fff">
-    <!-- Standard iPad Touch Icon-->
-    <link rel="apple-touch-icon" sizes="72x72" href="http://placehold.it/72.png/000/fff">
-    <!-- Standard iPhone Touch Icon-->
-    <link rel="apple-touch-icon" sizes="57x57" href="http://placehold.it/57.png/000/fff">
-    <!-- Styles -->
-    <link href="{{ asset('assets/css/lib/font-awesome.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/lib/themify-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/lib/owl.theme.default.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/lib/menubar/sidebar.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/lib/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/lib/helper.css') }}" rel="stylesheet">
-    {{-- my style --}}
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/admin-styled.css') }}" rel="stylesheet">
+        :root {
+            --tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+        }
 
+        body {
+            font-feature-settings: "cv03", "cv04", "cv11";
+        }
+    </style>
 </head>
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,38 +1,26 @@
-<!DOCTYPE html>
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta19
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
 <html lang="en">
+
 @include('admin.components.head')
 
-<body class="dark">
-    @include('admin.components.link-script')
-    <div>
+<body>
+    <script src="{{ asset('vendor/tabler/tabler.min.js') }}" defer></script>
+    <div class="page">
         @include('admin.components.navbar')
-        @include('admin.components.header')
-        <div class="content-wrap">
-            <div class="main">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-lg-4 p-l-0 title-margin-left">
-                            <div class="page-header">
-                                <div class="page-title">
-                                    <ol class="breadcrumb p-0">
-                                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
-                                        @if (isset($page))
-                                            <li class="breadcrumb-item active">{{ $page }}</li>
-                                        @endif
-                                        @if (isset($action))
-                                            <li class="breadcrumb-item active">{{ $action }}</li>
-                                        @endif
+        <div class="page-wrapper">
+            @yield('content')
 
-                                    </ol>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    @yield('content')
-                </div>
-            </div>
         </div>
     </div>
+    @include('admin.components.link-script')
 </body>
 
 </html>
