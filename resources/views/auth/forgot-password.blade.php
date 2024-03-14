f387d4c0564e820fda3e4cdf34066ce0efb095c3
lequanphat <lequanphat3579@gmail.com>
2024-03-14 18:31:40 +0700

    auth page using tabler

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,18 +1,34 @@
 @extends('layouts.empty')
 @section('content')
-    <div class="container rounded bg-white p-4 " style="width: 400px;">
-        <h1 class='text-center fs-4 '>FORGOT PASSWORD</h1>
-        <p class="m-0 mt-3">Enter the email associated with your account, we will send a confirmation code to your email.</p>
-        <form action="/forgot-password" method="post">
-            @csrf
-            <input type="text" name="email" id="email" value="{{ old('email') }}" placeholder="example@gmail.com"
-                class="form-control mt-1 mb-3" required>
+    <form class="card card-md" action="/forgot-password" method="post" autocomplete="off" novalidate>
+        @csrf
+        <div class="card-body">
+            <h2 class="h2 card-title text-center mb-4">Forgot password</h2>
+            <p class="text-muted mb-4">Enter your email address and your password will be reset and emailed to you.</p>
+            <div class="mb-3">
+                <label class="form-label">Email address</label>
+                <input type="text" name="email" id="email" value="{{ old('email') }}" class="form-control"
+                    placeholder="Enter email">
+            </div>
             @if ($errors->any())
                 <p class="text-danger m-0">*{{ $errors->first() }}</p>
             @endif
-            <button type="submit" class="btn btn-primary container-fluid ">SUBMIT</button>
-            <p class="m-0 mt-3 text-center">Return to login? <a class="text-primary" href="/login">Login</a>
-            </p>
-        </form>
+            <div class="form-footer">
+                <button type="submit" class="btn btn-primary w-100">
+                    <!-- Download SVG icon from http://tabler-icons.io/i/mail -->
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                        stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+                        <path d="M3 7l9 6l9 -6" />
+                    </svg>
+                    Send me new password
+                </button>
+            </div>
+        </div>
+    </form>
+    <div class="text-center text-muted mt-3">
+        Forget it, <a href="/login">send me back</a> to the sign in screen.
     </div>
 @endsection
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,75 @@
 @extends('layouts.empty')
 @section('content')
-    <div class="container rounded bg-white p-4 " style="width: 400px;">
-        <h1 class='text-center fs-2'>LOGIN</h1>
-        <form action="/login" method="post">
-            @csrf
-            <label for="email" class="mt-2 ">Email:</label>
-            <input type="text" name="email" id="email" value="{{ old('email') }}" class="form-control" required>
-            <label for="password" class="mt-2 ">Password:</label>
-            <input type="password" name="password" id="password" value="{{ old('password') }}" class="form-control mb-4"
-                required>
-            @if ($errors->any())
-                <p class="text-danger m-0">*{{ $errors->first() }}</p>
-            @endif
-
-            <button type="submit" class="btn btn-primary container-fluid ">LOGIN</button>
-            <p class="text-center my-2"><a class="text-primary " href="/forgot-password">Forgot password</a></p>
-            <div class="border-top-1px my-3"></div>
-            <p class="m-0 text-center">You don't have an account? <a class="text-primary" href="/register">Register</a>
-            </p>
-        </form>
+    <div class="card card-md">
+        <div class="card-body">
+            <h2 class="h2 text-center mb-4">Login to your account</h2>
+            <form action="/login" method="post" autocomplete="off">
+                @csrf
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control"
+                        placeholder="your@email.com" autocomplete="off" required>
+                </div>
+                <div class="mb-2">
+                    <label for="password" class="form-label">
+                        Password
+                        <span class="form-label-description">
+                            <a href="/forgot-password">I forgot password</a>
+                        </span>
+                    </label>
+                    <div class="input-group input-group-flat">
+                        <input type="password" name="password" id="password" value="{{ old('password') }}"
+                            class="form-control" placeholder="Your password" autocomplete="off" required>
+                        <span class="input-group-text">
+                            <a href="#" class="link-secondary" title="Show password"
+                                data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                    viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                    stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                    <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+                                    <path
+                                        d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+                                </svg>
+                            </a>
+                        </span>
+                    </div>
+                </div>
+                @if ($errors->any())
+                    <p class="text-danger m-0">*{{ $errors->first() }}</p>
+                @endif
+                <div class="form-footer">
+                    <button type="submit" class="btn btn-primary w-100">Sign in</button>
+                </div>
+            </form>
+        </div>
+        <div class="hr-text">or</div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col"><a href="#" class="btn w-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon text-github" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path
+                                d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+                        </svg>
+                        Login with Github
+                    </a></div>
+                <div class="col"><a href="#" class="btn w-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon text-twitter" width="24" height="24"
+                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                            stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path
+                                d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c0 -.249 1.51 -2.772 1.818 -4.013z" />
+                        </svg>
+                        Login with Twitter
+                    </a></div>
+            </div>
+        </div>
+    </div>
+    <div class="text-center text-muted mt-3">
+        Don't have account yet? <a href="/register" tabindex="-1">Sign up</a>
     </div>
 @endsection
