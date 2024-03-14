f0f149cea2339bf26789769b60dc2a4eb8076d8e
lequanphat <lequanphat3579@gmail.com>
2024-03-14 20:16:51 +0700

    user ui using tabler

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -28,13 +28,8 @@
                 <ul class="list-inline list-inline-dots mb-0">
                     <li class="list-inline-item">
                         Copyright &copy; 2023
-                        <a href="." class="link-secondary">Tabler</a>.
+                        <a href="#" class="link-secondary">QuanPhat</a>.
                         All rights reserved.
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="./changelog.html" class="link-secondary" rel="noopener">
-                            v1.0.0-beta19
-                        </a>
                     </li>
                 </ul>
             </div>
