fb5ea44f21984110e1a1c6b6b51df83dc31c433d
omodtung <dothedung131@gmail.com>
2024-03-29 00:16:06 +0700

    search with title

--- resources/views/admin/discounts/discountUI.blade.php
+++ resources/views/admin/discounts/discountUI.blade.php
@@ -57,7 +57,8 @@
 
                 </div>
 
-                <input type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+                <input  id="myInput" onkeyup="myFunction()"
+                    type="text" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
             </div>
         </div>
         <div class="page-body">
@@ -66,7 +67,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="table-responsive">
-                                <table class="table table-vcenter card-table">
+                                <table id="myTable" class="table table-vcenter card-table">
                                     <thead>
 
                                     <tr>
@@ -161,6 +162,31 @@
                     </div>
                 </div>
             </div>
+            <script>
+                function myFunction() {
+                    // Declare variables
+                    var input, filter, table, tr, td, i, txtValue;
+                    input = document.getElementById("myInput");
+                    filter = input.value.toUpperCase();
+                    table = document.getElementById("myTable");
+                    tr = table.getElementsByTagName("tr");
+
+                    // Loop through all table rows, and hide those who don't match the search query
+                    for (i = 0; i < tr.length; i++) {
+                        td = tr[i].getElementsByTagName("td")[1];
+                        // fast find follow title [1];
+                        // Tip: Change tr[i].getElementsByTagName('td')[0] to [1] if you want to search for "Country" (index 1) instead of "Name" (index 0).
+                        if (td) {
+                            txtValue = td.textContent || td.innerText;
+                            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                                tr[i].style.display = "";
+                            } else {
+                                tr[i].style.display = "none";
+                            }
+                        }
+                    }
+                }
+            </script>
             @include('admin.discounts.CreateDiscount')
             @include('admin.discounts.UpdateDiscount')
             @include('admin.discounts.deleteDiscount')
