c600252060705c4fef087fc3949e5790136b092b
lequanphat <lequanphat3579@gmail.com>
2024-03-29 20:27:38 +0700

    resolve conflict

--- routes/web.php
+++ routes/web.php
@@ -153,11 +153,6 @@ Route::middleware([AdminMiddleware::class])->group(function () {
     // *This is only temporary, use the appropriate controller
 
 
-    Route::get('/admin/orders', [PagesController::class, 'admin_orders']);
-
-    //    Route::get('/admin/discounts', [PagesController::class, 'admin_discounts']);
-    Route::get('/admin/orders', [PagesController::class, 'admin_orders']);
-
     Route::get('/admin/warranties', [PagesController::class, 'admin_warranties']);
     Route::get('/admin/receipts', [PagesController::class, 'admin_receipts']);
     Route::get('/admin/permissions', [PagesController::class, 'admin_permissions']);
