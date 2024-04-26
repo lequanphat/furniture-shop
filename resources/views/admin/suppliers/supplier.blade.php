744a4f60833de6538b34b9cebda613b14105dcae
lequanphat <lequanphat3579@gmail.com>
2024-04-26 08:11:41 +0700

    fix sort waranty features

--- resources/views/admin/suppliers/supplier.blade.php
+++ resources/views/admin/suppliers/supplier.blade.php
@@ -15,7 +15,7 @@
                 <!-- Page title actions -->
                 <div class="col-auto ms-auto d-print-none">
                     <div class="btn-list">
-                    <div class="input-icon ">
+                        <div class="input-icon ">
                             <input id="search-supplier-input" type="text" value="{{ $search }}" class="form-control"
                                 placeholder="Search…">
                             <span class="input-icon-addon">
@@ -89,29 +89,36 @@
                                             <td>{{ $supplier->phone_number }}</td>
                                             {{-- temporary value --}}
                                             <td>
-                                                <button type="button" class="js-update-category-btn btn  mr-2 px-2 py-1"
+                                                <button type="button" class="js-update-category-btn btn  p-2"
                                                     data-bs-toggle="modal" data-bs-target="#UpdateSupplierModal"
                                                     data-supplier-id="{{ $supplier->supplier_id }}"
                                                     data-name="{{ $supplier->name }}"
                                                     data-description="{{ $supplier->description }}"
                                                     data-address="{{ $supplier->address }}"
                                                     data-phone-number="{{ $supplier->phone_number }}">
-                                                    <img src="{{ asset('svg/edit.svg') }}" style="width: 18px;" />
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="action-btn-icon icon icon-tabler icons-tabler-outline icon-tabler-pencil">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
+                                                        <path d="M13.5 6.5l4 4" />
+                                                    </svg>
                                                 </button>
-                                                <button data-bs-toggle="modal" data-bs-target="#delete-confirm-modal" data-supplier-id="{{ $supplier->supplier_id }}" class="btn p-2">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24"
-                    height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="action-btn-icon icon icon-tabler icons-tabler-outline icon-tabler-trash">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                    <path d="M4 7l16 0" />
-                    <path d="M10 11l0 6" />
-                    <path d="M14 11l0 6" />
-                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                </svg>
-            </button>
+                                                <button data-bs-toggle="modal" data-bs-target="#delete-confirm-modal"
+                                                    data-supplier-id="{{ $supplier->supplier_id }}" class="btn p-2">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                                        stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                                        class="action-btn-icon icon icon-tabler icons-tabler-outline icon-tabler-trash">
+                                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M4 7l16 0" />
+                                                        <path d="M10 11l0 6" />
+                                                        <path d="M14 11l0 6" />
+                                                        <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                                        <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                                    </svg>
+                                                </button>
 
                                             </td>
                                     @endforeach
